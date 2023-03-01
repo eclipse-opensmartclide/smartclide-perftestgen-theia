1cdcda94b0fc6e6f3c064a7be642a1a37fd2ef45
David García Folguera <77740626+davidgarcia-kairosds@users.noreply.github.com>
2023-03-01 15:38:31 +0100

    Added sidecar service component. Adjusted dependabot and github actions (#38)
    * Added sidecar service component. Adjusted dependabot and github actions
    
    * fixed auto build

---
+++ sidecar_service/src/test/java/smartclide/perftestgen/PerftestGenServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package smartclide.perftestgen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerftestGenServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
